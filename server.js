24e786b737ea9f30738daa2e1712d13a2c2276a6
Eric Lewis <eric.andrew.lewis@gmail.com>
2018-06-26 08:40:46 -0400

    Add production static file serving setup

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ require('dotenv').config()
 // Get the express module.
 const express = require('express');
 const { createClient } = require('mta-realtime-subway-departures');
+const path = require('path');
 
 // Create a new Express application (web server)
 const app = express();
+
+app.use("/static", express.static("./build/static/"));
+
 
 const client = createClient(process.env.MTA_API_KEY);
 
@@ -17,7 +21,16 @@ app.get('/api/departures.json', (request, response) => {
   client.departures(complexIds).then(departuresResponse => {
     response.json(departuresResponse);
   });
-})
+});
+
+// In production, any request that doesn't match a previous route
+// should send the front-end application, which will handle the route.
+if (process.env.NODE_ENV == "production") {
+  app.get("/*", function(request, response) {
+    response.sendFile(path.join(__dirname, "build", "index.html"));
+  });
+}
+
 
 // Start the web server listening on the provided port.
 app.listen(PORT, () => { 
