dd550d1e46df36a0037791ba004e65855b892d74
Eric Lewis <eric.andrew.lewis@gmail.com>
2018-06-27 07:35:57 -0400

    Add debug locations

--- src/App/index.js
+++ src/App/index.js
@@ -2,6 +2,36 @@ import React, { Component } from "react";
 import Geolocation from "react-geolocation";
 import SubwayDeparturesPage from '../SubwayDeparturesPage';
 import "./style.css";
+const locations = [
+  {
+    lat: 40.6872257,
+    long: -73.9737066
+  },
+  {
+    lat: 40.7359,
+    long: -73.9911
+  },
+  // Franklin Ave Shuttle
+  {
+    lat: 40.678211,
+    long: -73.9596304
+  },
+  // Times Square
+  {
+    lat: 40.7542647,
+    long: -73.98682
+  },
+  // Flushing Main St
+  {
+    lat: 40.7595582,
+    long: -73.8321161
+  },
+  // Rockaway
+  {
+    lat: 40.5805469,
+    long: -73.8396189
+  },
+];
 
 class App extends Component {
   render() {
@@ -29,8 +59,8 @@ class App extends Component {
                 </div>
               }
               {!fetchingPosition && !error && process.env.NODE_ENV === 'development' && <SubwayDeparturesPage
-                latitude={40.7359}
-                longitude={-73.9911}
+                latitude={locations[5].lat}
+                longitude={locations[5].long}
               />}
               {!fetchingPosition && !error && process.env.NODE_ENV === 'production' && <SubwayDeparturesPage
                 latitude={latitude}
